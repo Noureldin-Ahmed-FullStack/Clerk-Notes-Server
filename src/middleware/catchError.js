44c52479de91dbeb6a4ad15fe2f98dfb3ad7f65f
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-06-14 16:11:08 +0300

    logged errors

--- src/middleware/catchError.js
+++ src/middleware/catchError.js
@@ -1,5 +1,5 @@
 export function catchError(func){
     return(req,res,next)=>{
-        func(req,res,next).catch(err =>{next(err)})
+        func(req,res,next).catch(err =>{res.status(404).json({ message: err })})
     }
 } 
